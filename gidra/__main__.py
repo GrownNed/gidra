c997b337a68ce036c7d2570f46bfe401697e3fa5
ayy lmao <ridvan-nuri@windowslive.com>
2022-07-01 11:57:53 +0300

    Change ips to localhost

--- gidra/__main__.py
+++ gidra/__main__.py
@@ -20,7 +20,7 @@ def handle_query_cur():
     proto.parse(base64.b64decode(r.text))
 
     if proto.retcode == 0:
-        proto.region_info.gateserver_ip = '192.168.1.71'
+        proto.region_info.gateserver_ip = '127.0.0.1'
         proto.region_info.gateserver_port = 8888
         proxy.key = ec2b.derive(proto.client_secret_key)
 
@@ -28,7 +28,7 @@ def handle_query_cur():
 
 def main():
     global proxy
-    proxy = GenshinProxy(('192.168.1.71', 8888), ('47.245.143.151', 22102))
+    proxy = GenshinProxy(('127.0.0.1', 8888), ('47.245.143.151', 22102))
 
     proxy.add(change_account.router)
     proxy.add(change_nickname.router)
@@ -37,7 +37,7 @@ def main():
 
     proxy.start()
 
-    run(host='192.168.1.71', port=8081, debug=False)
+    run(host='127.0.0.1', port=8081, debug=False)
 
 if __name__ == '__main__':
     main()
